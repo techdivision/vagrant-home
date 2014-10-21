f4bd72bce7c376187d0e84b77793c834c0bef8e2
Robert Lemke <rl@robertlemke.com>
2014-10-21 11:03:01 +0200

    Updated cookbooks

--- chef/cookbooks/techdivision-webserver/attributes/default.rb
+++ chef/cookbooks/techdivision-webserver/attributes/default.rb
@@ -6,7 +6,7 @@ default["php-fpm"]["pools"] = [
   }
 ]
 
-default['nginx']['configure_flags'] = ['--add-module=/tmp/ngx_http_redis-0.3.7']
-default['nginx']['source']['version']  = "1.7.6"
-default['nginx']['source']['url']      = "http://nginx.org/download/nginx-#{node['nginx']['source']['version']}.tar.gz"
+default['nginx']['configure_flags'] = ["--add-module=#{Chef::Config['file_cache_path']}/ngx_http_redis-0.3.7"]
+default['nginx']['source']['version'] = "1.7.6"
+default['nginx']['source']['url'] = "http://nginx.org/download/nginx-#{node['nginx']['source']['version']}.tar.gz"
 default['nginx']['source']['checksum'] = '08e2efc169c9f9d511ce53ea16f17d8478ab9b0f7a653f212c03c61c52101599'
